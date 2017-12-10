dd50b8aa0e6bbc32d392ff8313fa6f4de3fcbfda
Shona Doran <shonamdoran@gmail.com>
2017-12-10 19:43:12 +0100

    Background texture for the front desk has been added

---
+++ WitchGame/Assets/FrontDesk/Scripts/FrontDesk.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script will be attatched to the frontdesk scene and the main camera
+/// </summary>
+
+public class FrontDesk : MonoBehaviour {
+
+    public Texture backgroundtexture ;
+
+    private void OnGUI()
+    {
+        ///Display the texture created for the front desk
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundtexture);
+    }
+
+
+}
