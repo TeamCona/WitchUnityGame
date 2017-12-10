f98e08aa7f585364fe12364453937cd0b05e2166
Shona Doran <shonamdoran@gmail.com>
2017-12-10 20:52:27 +0100

    Added two buttons that as of yet don't contain functionality

--- WitchGame/Assets/FrontDesk/Scripts/FrontDesk.cs
+++ WitchGame/Assets/FrontDesk/Scripts/FrontDesk.cs
@@ -9,11 +9,28 @@ using UnityEngine;
 public class FrontDesk : MonoBehaviour {
 
     public Texture backgroundtexture ;
+    public Texture cal;
 
     private void OnGUI()
     {
         ///Display the texture created for the front desk
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundtexture);
+
+        //Create the buttons that will change the scene or location of the game
+        //First button
+        //This one uses a string
+        if (GUI.Button (new Rect(Screen.width *.5f, 0, Screen.width * .2f, Screen.height* .1f), "Ingredients"))
+        {
+            print("Ingredients Button works");
+        }
+
+        //Second button
+        //This one works without a gui outline meaning it uses a picture instead of a string
+        //Uses temporary picture
+        if (GUI.Button (new Rect(Screen.width * .75f, 0, Screen.width * .2f, Screen.height * .1f),cal, ""))
+        {
+            print("Kitchen Button works");
+        }
     }
 
 
