fdbfb31b0ac87570c03bee2caa416612e54066a0
Shona Doran <shonamdoran@gmail.com>
2017-12-11 16:39:23 +0100

    Started change scene test

---
+++ WitchGame/Assets/TestChange/Scripts/TestChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestChange : MonoBehaviour
+{
+
+    public Texture backgroundtexture;
+
+    private void OnGUI()
+    {
+        ///Display the texture created for the front desk
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundtexture);
+    }
+}
