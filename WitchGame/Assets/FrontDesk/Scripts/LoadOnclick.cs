7169eb3343fe89561410345f9062decbbee20e04
Shona Doran <shonamdoran@gmail.com>
2017-12-12 16:19:00 +0100

    Added some images for the upgrade shop scene and added some comments for the code

--- WitchGame/Assets/FrontDesk/Scripts/LoadOnclick.cs
+++ WitchGame/Assets/FrontDesk/Scripts/LoadOnclick.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// This script will be used in the work day scenes to allow the player to switch between screens
+/// </summary>
+
 public class LoadOnclick : MonoBehaviour {
 
+    /// <summary>
+    /// This function will activate when a button is clicked. The button will then pass the name of the scene it will load into scenemanger
+    /// which will change the scene
+    /// </summary>
+    /// <param name="scenename"></param>
 	public void LoadScene(string scenename)
     {
         SceneManager.LoadScene(scenename);
