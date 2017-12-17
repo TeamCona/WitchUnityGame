0683ddaa992ea9b8f6f3c5f807acbf94de0700ae
Shona Doran <shonamdoran@gmail.com>
2017-12-17 20:39:55 +0100

    Had some issues with a scene but have remade it and have a new one working

---
+++ WitchGame/Assets/FDesk/Scripts/FDesk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FDesk : MonoBehaviour {
+
+    public int potionneed1, potionneed2;
+    public float maintimer = 10.0f;
+
+    // Use this for initialization
+    void Start () {
+
+        potionneed1 = 1;
+        potionneed2 = 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        maintimer -= Time.deltaTime;
+
+        if (maintimer <= 0)
+        {
+            updatenum();
+        }
+    }
+
+    private void updatenum()
+    {
+        print("Works");
+        maintimer = 10.0f;
+    }
+}
