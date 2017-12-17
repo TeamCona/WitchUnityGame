ba34df397801698341e11343ac19baaf4ee5989d
Shona Doran <shonamdoran@gmail.com>
2017-12-17 19:42:58 +0100

    Attempting timer

--- WitchGame/Assets/FrontDesk/Scripts/FrontDesk.cs
+++ WitchGame/Assets/FrontDesk/Scripts/FrontDesk.cs
@@ -2,9 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FrontDesk : MonoBehaviour {
-    
-    //This scene will have the manager speak to the player and update the amount of poyions needed to make
+public class FrontDesk : MonoBehaviour
+{
+    public int num;
+    public float maintimer = 10.0f;
 
+    private void Start()
+    {
+        num = 0;
+    }
 
+    private void Update()
+    {
+        maintimer -= Time.deltaTime;
+
+        if(maintimer <= 0)
+        {
+            updatenum();
+        }
+    }
+
+    private void updatenum()
+    {
+        print("Works");
+        maintimer = 10.0f;
+    }
 }
