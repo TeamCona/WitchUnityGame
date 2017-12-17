cc3dbde2d3c11cd281d177fa958706078594ed81
Shona Doran <shonamdoran@gmail.com>
2017-12-17 21:01:21 +0100

    Added another timer that will track the day and when ending will change scene to the end of day scene

--- WitchGame/Assets/FDesk/Scripts/FDesk.cs
+++ WitchGame/Assets/FDesk/Scripts/FDesk.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class FDesk : MonoBehaviour {
 
     public int potionneed1, potionneed2;
+    public float daytimer = 30.0f;
     public float maintimer = 10.0f;
 
+    /// <summary>
+    /// This function is called at the start of the scene to initialise the first potions. For the starter level, we will only include the 2
+    /// </summary>
     // Use this for initialization
     void Start () {
 
@@ -15,12 +21,25 @@ public class FDesk : MonoBehaviour {
     }
 	
 	// Update is called once per frame
+    /// <summary>
+    /// This function is called everyframe and will use the floats initialised earlier to countdown until the either you may check for more potions or
+    /// if the day ends
+    /// </summary>
 	void Update () {
+        daytimer -= Time.deltaTime;
         maintimer -= Time.deltaTime;
 
-        if (maintimer <= 0)
+        if (daytimer <= 0)
         {
-            updatenum();
+            SceneManager.LoadScene("UpgradeShop");
+        }
+
+        else
+        {
+            if (maintimer <= 0)
+            {
+                updatenum();
+            }
         }
     }
 
