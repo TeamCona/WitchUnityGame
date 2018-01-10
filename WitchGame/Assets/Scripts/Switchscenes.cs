f3fd2c52a70c7b9cd0c4c69015ff656c0eab38d7
ciara.sastre <ciara.sastre@stud.h-da.de>
2018-01-10 20:46:20 +0000

    Added Mona to Cut Scene

--- WitchGame/Assets/Scripts/Switchscenes.cs
+++ WitchGame/Assets/Scripts/Switchscenes.cs
@@ -7,7 +7,7 @@ public class Switchscenes : MonoBehaviour {
 
 	public void PlayGame()
 	{
-		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		SceneManager.LoadScene ("Scenes/FDesk");
 	}
 
 	public void Instructions()
