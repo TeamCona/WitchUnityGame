a5ea9a8f294b23215fc7bbdc61a3006d7ba6541b
Shona Doran <shonamdoran@gmail.com>
2018-01-12 22:33:29 +0100

    Added music that transitions between scenes and loops

---
+++ WitchGame/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+	/// <summary>
+    /// This function will add the looping music to the game
+    /// </summary>
+	void Awake ()
+    {
+        //Function will find objects that were tagged and will destroy one to prevent 2 songs playing at once 
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+
+        if(objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        //Allows the music to keep looping
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+	
+}
