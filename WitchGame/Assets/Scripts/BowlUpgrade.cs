4c50c447403ad1a55ae156d2d1c8edb6cf685ce7
Shona Doran <shonamdoran@gmail.com>
2018-01-17 20:14:15 +0100

    Started the upgrades

---
+++ WitchGame/Assets/Scripts/BowlUpgrade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BowlUpgrade : MonoBehaviour
+{
+
+    Image bowlImage;
+
+    public Sprite potion3;
+    public Sprite potion4;
+
+    // Use this for initialization
+    void Start () {
+        UpgradeBowl();
+	}
+
+    void UpgradeBowl()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
