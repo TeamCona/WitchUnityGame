a9308b85d422b28ae7d4ace67a7e9138c98da405
Shona Doran <shonamdoran@gmail.com>
2018-01-17 19:37:00 +0100

    Added potions that change depending on day

---
+++ WitchGame/Assets/Scripts/UpgradeShop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeShop : MonoBehaviour {
+
+    Image PotionImage;
+
+    public Sprite potion3;
+    public Sprite potion4;
+    
+	// Use this for 
+    /// <summary>
+    /// Will automatically change the potion to match the day
+    /// </summary>
+	void Start ()
+    {
+        ChangePotion();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ChangePotion()
+    {
+        if(FDesk.day == 2)
+        {
+            gameObject.GetComponent<Image>().sprite = potion3;
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().sprite = potion4;
+        }
+    }
+}
