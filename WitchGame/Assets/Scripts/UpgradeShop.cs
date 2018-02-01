babdbceb903ae5e341748f3163e1935e3f7aab14
Shona Doran <shonamdoran@gmail.com>
2018-02-01 23:07:08 +0100

    Started money code and art

--- WitchGame/Assets/Scripts/UpgradeShop.cs
+++ WitchGame/Assets/Scripts/UpgradeShop.cs
@@ -9,6 +9,8 @@ public class UpgradeShop : MonoBehaviour {
 
     public Sprite potion3;
     public Sprite potion4;
+	public Text MoneyText;
+	public int money;
     
 	// Use this for 
     /// <summary>
@@ -32,5 +34,8 @@ public class UpgradeShop : MonoBehaviour {
         {
             gameObject.GetComponent<Image>().sprite = potion4;
         }
+
+		money = Fulfil.cashmonay;
+		MoneyText.text = money.ToString();
     }
 }
