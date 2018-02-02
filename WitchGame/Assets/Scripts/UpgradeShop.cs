5a069c316843611551a9dd64842e3ca5c1473817
Shona Doran <shonamdoran@gmail.com>
2018-02-02 16:06:58 +0100

    Added some art and code to the Upgrade shop

--- WitchGame/Assets/Scripts/UpgradeShop.cs
+++ WitchGame/Assets/Scripts/UpgradeShop.cs
@@ -5,12 +5,20 @@ using UnityEngine.UI;
 
 public class UpgradeShop : MonoBehaviour {
 
-    Image PotionImage;
-
+	//Sprites containing new potions to make
     public Sprite potion3;
     public Sprite potion4;
+
+	//Text showing amounts of money on the GUI
 	public Text MoneyText;
+	public Text CostText1;
+	public Text CostText2;
+
+	//Amount of money owned and needed for purchases
 	public int money;
+	public int cost1;
+	public int cost2;
+	public int upgrade1, upgrade2;
     
 	// Use this for 
     /// <summary>
@@ -19,10 +27,12 @@ public class UpgradeShop : MonoBehaviour {
 	void Start ()
     {
         ChangePotion();
+		UpdateMoney();
+		UpdateCost ();
 	}
 
     /// <summary>
-    /// Will check the day and will add the potion for that day
+    /// Will check the day and will add the potion for use the next day
     /// </summary>
     void ChangePotion()
     {
@@ -34,8 +44,42 @@ public class UpgradeShop : MonoBehaviour {
         {
             gameObject.GetComponent<Image>().sprite = potion4;
         }
+    }
 
+	/// <summary>
+	/// Function will update the text that shows monwy availible
+	/// </summary>
+	void UpdateMoney()
+	{
 		money = Fulfil.cashmonay;
 		MoneyText.text = money.ToString();
-    }
+	}
+
+	void UpdateCost()
+	{
+		//Upgrade1
+		if (upgrade1 < 1) 
+		{
+			cost1 = 4;
+		}
+		if (upgrade1 == 1) 
+		{
+			cost1 = 7;
+		}
+
+		//Upgrade2
+		if (upgrade2 < 1) 
+		{
+			cost2 = 5;
+		}
+		if (upgrade2 == 1) 
+		{
+			cost2 = 9;
+		}
+
+		//Change cost text
+
+		CostText1.text = cost1.ToString ();
+		CostText2.text = cost2.ToString ();
+	}
 }
