614573c3ac92d972bf2b9f9252d573b2d38e29e1
Shona Doran <shonamdoran@gmail.com>
2018-02-04 17:35:31 +0100

    Merge, added a button script

---
+++ WitchGame/Assets/Scripts/FinishDayClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishDayClick : MonoBehaviour 
+{
+	/// <summary>
+	/// This fuction will check conditions when the button has been clicked.
+	/// If the game is over it will change to the cutscene 
+	/// If not it will increment the day count and go back to the front desk scene
+	/// </summary>
+	public void FinishDay()
+	{
+		if (FDesk.day == 3) 
+		{
+			Debug.Log ("Add ending cutsene here");
+		} 
+
+		else 
+		{
+			Debug.Log ("Day thing works");
+			FDesk.day++;
+			SceneManager.LoadScene("FDesk");
+		}
+	}
+}
