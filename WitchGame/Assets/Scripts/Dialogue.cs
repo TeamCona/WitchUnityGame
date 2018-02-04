29fe97acc7e404126ad7df084b865de36d981f70
ciara.sastre <ciara.sastre@stud.h-da.de>
2018-02-04 16:47:00 +0100

    Added 2 new scripts

---
+++ WitchGame/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue : MonoBehaviour {
+
+	public string name;
+
+	/// <summary>
+	/// Text area increases length of
+	/// sentences that can be written.
+	///  </summary>
+	[TextArea(3, 10)]
+	public string[] sentences;
+}
