ac93586bc46319d13838621f1a1651ed552141bc
ciara.sastre <ciara.sastre@stud.h-da.de>
2018-02-05 10:11:51 +0100

    Chat Fixed

---
+++ WitchGame/Assets/Scripts/ChatManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatManager : MonoBehaviour {
+
+	private Queue<string> sentences;
+
+	/// <summary>
+	/// Initializing new queue of
+	/// sentences
+	/// </summary>
+	void Start () {
+		sentences = new Queue<string> ();
+	}
+
+	public void StartChat(Chat chat)
+	{
+		Debug.Log ("Starting chat" + chat.name);
+	}
+
+}
