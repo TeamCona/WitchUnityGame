7ae4ade9ac399360a67b08dc0eca9ed7cea35105
ciara.sastre <ciara.sastre@stud.h-da.de>
2018-02-05 10:31:44 +0100

    Added sentence cycle

--- WitchGame/Assets/Scripts/ChatManager.cs
+++ WitchGame/Assets/Scripts/ChatManager.cs
@@ -14,9 +14,42 @@ public class ChatManager : MonoBehaviour {
 		sentences = new Queue<string> ();
 	}
 
+	/// <summary>
+	/// Clears sentences
+	/// Then Enqueues the next sentence to be used
+	/// </summary>
+	/// <param name="chat">Chat.</param>
 	public void StartChat(Chat chat)
 	{
 		Debug.Log ("Starting chat" + chat.name);
+
+
+		sentences.Clear ();
+
+		foreach (string sentence in chat.sentences) {
+			sentences.Enqueue (sentence);
+		}
+
+		DisplayNextSentence ();
+	}
+
+	public void DisplayNextSentence()
+	{
+		if (sentences.Count == 0) {
+			EndChat ();
+			return;
+		}
+
+		string sentence = sentences.Dequeue ();
+		Debug.Log (sentence);
+	}
+
+	/// <summary>
+	/// Ends the chat.
+	/// </summary>
+	void EndChat()
+	{
+		Debug.Log ("Chat ended");
 	}
 
 }
