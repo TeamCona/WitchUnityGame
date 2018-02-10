66be7ceb0ff012dca10635f4d0c3030486a79006
Shona Doran <shonamdoran@gmail.com>
2018-02-10 16:01:01 +0100

    Started work on potion bottle, can now move and change colour

---
+++ WitchGame/Assets/Scripts/PotionDrag.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionDrag : MonoBehaviour {
+
+	//Add Sprites
+	public Sprite EmptyPotion;
+
+	//This section is used to enable the drag feature
+	float distance = 10;
+
+	void OnMouseDrag()
+	{
+		//Need to save previous Position
+		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+		transform.position = objPosition;
+	}
+
+	/*void OnMouseUp()
+	{
+		transform.position = new Vector3 (0, 0, 0);
+	}*/
+
+	//The ingredient has been dropped into the cauldron
+	void OnTriggerEnter2D()
+	{
+		Debug.Log ("Trigger touched");
+	}
+
+	void OnTriggerStay2D()
+	{
+		Debug.Log ("Still touching");
+	}
+
+	void OnTriggerExit2D()
+	{
+		Debug.Log ("Exit");
+	}
+}
---
+++ WitchGame/Assets/Scripts/PotionTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PotionTest : MonoBehaviour {
+
+	public Sprite Empty;
+	public Sprite Full;
+
+
+	/*float timer = 1f;
+	float delay = 1f;*/
+
+	void Start()
+	{
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = Empty;
+	}
+
+	/*void Update()
+	{
+		timer -= Time.deltaTime;
+		if (timer <= 0) 
+		{
+			if (this.gameObject.GetComponent<SpriteRenderer> ().sprite == Empty) 
+			{
+				this.gameObject.GetComponent<SpriteRenderer> ().sprite = Full;
+				timer = delay;
+				return;
+			}
+			if (this.gameObject.GetComponent<SpriteRenderer> ().sprite == Full) 
+			{
+				this.gameObject.GetComponent<SpriteRenderer> ().sprite = Empty;
+				timer = delay;
+				return;
+			}
+		}
+
+	}*/
+
+	//This section is used to enable the drag feature
+	float distance = 10;
+
+	void OnMouseDrag()
+	{
+		//Need to save previous Position
+		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+		transform.position = objPosition;
+
+		Debug.Log ("Drag should work");
+	}
+		
+}
