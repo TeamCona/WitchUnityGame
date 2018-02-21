6e725dc4bad93fcbd6e302d4ba9e6590518c1212
Shona Doran <shonamdoran@gmail.com>
2018-02-21 12:45:27 +0100

    Merge

---
+++ WitchGame/Assets/Tests/Test1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class Test1 {
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator Test1WithEnumeratorPasses() {
+
+		SetupScene ();
+		yield return new WaitForSeconds(3);
+	}
+
+	public void SetupScene()
+	{
+		GameObject Potion = GameObject.FindWithTag("Potion");
+		MonoBehaviour.Instantiate(Potion) ;
+	}
+}
