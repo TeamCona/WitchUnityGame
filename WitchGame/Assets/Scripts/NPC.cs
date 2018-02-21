5b9551a7d213fde7a1ead804f2ff9c422a81a4a1
ciara.sastre <ciara.sastre@stud.h-da.de>
2018-02-21 22:21:28 +0100

    Open Scene Complete

--- WitchGame/Assets/Scripts/NPC.cs
+++ WitchGame/Assets/Scripts/NPC.cs
@@ -4,39 +4,22 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour {
 
-	/// <summary>
-	/// Connects the trigger and the
-	/// converation together
-	/// </summary>
 	public Chat chat;
 	public GameObject beginButton;
-	public GameObject monty;
-
-	/// <summary>
-	/// Removes the button.
-	/// </summary>
-	public void RemoveButton()
-	{
-		Destroy(beginButton.gameObject);
-	}
 
 	/// <summary>
 	/// Triggers the conversation.
 	/// </summary>
 	public void TriggerConversation()
 	{
-		
 		FindObjectOfType<ChatManager>().StartChat(chat);
 	}
 
-	public void StartAnimation()
+	public void RemoveButton()
 	{
-		if (gameObject.tag == "MontyDeter") 
-		{
-			monty = GameObject.FindGameObjectWithTag ("MontyDeter");
-			Instantiate (monty, transform.position, transform.rotation);
-
-			//monty = new Vector3(5.5, 1.2, 0);
-		}
+		beginButton = GameObject.FindGameObjectWithTag ("BeginButton");
+		//Destroy(beginButton.gameObject);
+		beginButton.SetActive (false);
 	}
+		
 }
---
+++ WitchGame/Assets/Scripts/OpenCutScene.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCutScene : MonoBehaviour {
+
+	public GameObject montyDeter;
+	public GameObject montyShock;
+	public GameObject catMShock;
+	public GameObject catM;
+	public GameObject Mona;
+	public int scene = 0;
+
+	/// <summary>
+	/// Start this instance.By setting all characters
+	/// invisable.
+	/// </summary>
+	void Start()
+	{
+		montyDeter = GameObject.FindGameObjectWithTag ("MontyDeter");
+		montyDeter.SetActive (false);
+
+		montyShock = GameObject.FindGameObjectWithTag ("MontyShock");
+		montyShock.SetActive (false);
+
+		catMShock = GameObject.FindGameObjectWithTag ("CatM_Shock");
+		catMShock.SetActive (false);
+
+		catM = GameObject.FindGameObjectWithTag ("CatM");
+		catM.SetActive (false);
+
+		Mona = GameObject.FindGameObjectWithTag ("Mona");
+		Mona.SetActive (false);
+	}
+
+	/// <summary>
+	/// Startings the scene.By Monty appearing
+	/// </summary>
+	public void StartingScene()
+	{
+		montyDeter.SetActive (true);
+		scene = 1;
+		Debug.Log ("Scene : " + scene);
+
+		scene = 2;
+	}
+
+	/// <summary>
+	/// Scene sequence starts characters appear
+	/// and dissapear when needed.
+	/// </summary>
+	public void Scenes()
+	{
+		if (scene == 2) 
+		{
+			montyDeter.SetActive (false);
+			montyShock.SetActive (true);
+			Debug.Log ("Scene : " + scene);
+		}
+		else
+		if (scene == 3) 
+		{
+			catMShock.SetActive (true);
+			montyShock.SetActive (false);
+			Debug.Log ("Scene : " + scene);
+		}
+		if (scene == 4) 
+		{
+			catMShock.SetActive (true);
+			Debug.Log ("Scene : " + scene);
+		}
+		if (scene == 5) 
+		{
+			catMShock.SetActive (true);
+			Debug.Log ("Scene : " + scene);
+		}
+		if (scene == 6) 
+		{
+			catMShock.SetActive (false);
+			Mona.SetActive(true);
+			catM.SetActive (true);
+			Debug.Log ("Scene : " + scene);
+		}
+
+		scene++;
+	}
+}
